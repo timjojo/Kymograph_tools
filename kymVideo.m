6823fab0a74e43970e3be9ffc404c581d8c26c10
Tim John Joseph <38778147+timjojo@users.noreply.github.com>
2024-06-13 13:14:59 -0400

    Added PNG indexed to RGB conversion to kymVideo
    Also updated kymVideo similar to kym_with_laser_and_ruler.m.
    
    Now indexed PNG files (grayscale with LUT) accepted. No need to first convert using MS Paint.
    RGB files also work.

--- kymVideo.m
+++ kymVideo.m
@@ -4,19 +4,17 @@
 % - Generates a rolling kymograph video 
 % - Rotates kymograph % to make horizontal if source kymograph is vertical.
 %
-% - Requires 3D matrix image. Open and save png in MS paint if PNG is 1D. 
-%   This will convert PNG to colour
 %
-%           Created: 6/11/2024, Updated: 6/12/2924, Tim John
+%           Created: 6/11/2024, Updated: 6/13/2924, Tim John
 %--------------------------------------------------------------------------
 
 
 %---------------------- Set these variables -------------------------------
-path_main_image = "18v2.png";
-path_laser_image = "18ch2v2.png";
+path_main_image = "18.png";
+path_laser_image = "18ch2.png";
 n_lines_per_frame = 180;
 frame_time = 100;           % in ms
-laser_on_high = 1;         % Change to -1 if value goes down on laser ON
+laser_on_high = -1;         % Change to -1 if value goes down on laser ON
 
 frame_width = 600;          % Frame height equals n_lines_per_frame
 start_index = 12000;                 
@@ -28,8 +26,17 @@ end_index = 22000;
 circle_time = frame_time/n_lines_per_frame;
 %--------------------------------------------------------------------------
 
-img_main = imread(path_main_image);
-img_laser = imread(path_laser_image);
+[img_main, map_main] = imread(path_main_image);
+[img_laser, map_laser] = imread(path_laser_image);
+
+if(~isempty(map_main))
+    img_main = ind2rgb(img_main, map_main);
+end
+
+sz_laser = size(img_laser);
+if(length(sz_laser) ~=3)
+    img_laser = cat(3, img_laser, img_laser, img_laser);
+end
 
 % Make horizontal
 sz = size(img_main);
@@ -50,6 +57,7 @@ sz = size(img_main);
 img_emptyBar = 0*ones(20,sz(2),sz(3));  % For seconds counter
 
 % Join laser to main kymograph
+img_laser = im2double(img_laser);      % Make data types uniform for proper concatenation
 imcomb = cat(1, img_main, img_laser(1:10,:,:), img_emptyBar);
 % figure
 imshow(imcomb(:,12000:22000,:));
